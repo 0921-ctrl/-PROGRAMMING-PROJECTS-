55e98e49bb79e461c5f88ece9513ac7fd52f9e11
0921-ctrl <raghavmittal262@gmail.com>
2024-11-06 20:51:09 +0530

    Add files via upload

---
+++ project 5  jarvis (PYTHON)/musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    
+    "namo" : "https://www.youtube.com/watch?v=dx4Teh-nv3A&pp=ygUEbmFtbw%3D%3D",
+    "let": "https://www.youtube.com/watch?v=HTcL9WkB_wg&pp=ygULbGV0IGhlciBnbyA%3D",
+    "maine":"https://www.youtube.com/watch?v=o7In7se2Kl4"
+}
